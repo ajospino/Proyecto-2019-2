17d0700d675768e3fa17c9fde1cf1fb2bec62173
LeurDieu <axlzbot@gmail.com>
2019-10-31 20:49:30 -0500

    Skere

--- Utiles/EnviarCorreo.py
+++ Utiles/EnviarCorreo.py
@@ -9,33 +9,14 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.encoders import encode_base64
 
-def mandarCorreoHtml(correo):
-    EMAIL_ADDRESS = EMAIL
-    EMAIL_PASSWORD = CONTRASEÑA
-
-    msg = EmailMessage()
-    msg['Subject'] = ASUNTO
-    msg['From'] = EMAIL_ADDRESS
-    msg['To'] = correo
-    msg.set_content(ASUNTO)
-
-    msg.add_alternative(html, subtype = 'html')
-
-    archivos = ['Codigos/TusCodigos.pdf']
-
-    for archivo in archivos:
-        with open(archivo, 'rb') as f:
-            file_data = f.read()
-            file_name = f.name
-
-    msg.add_attachment(file_data, maintype = 'aplication', subtype ='octect-stream', filename = file_name)
-
-
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-        smtp.send_message(msg)
-
-def mandarCorreoFactura(correo, pathArchivo):
+def enviarCorreo(tipo,correo, pathArchivo):
+    if(tipo == "FACTURA" or tipo =="NOTIF"):
+        path = pathArchivo
+    elif(tipo == "CODIGO"):
+        path = "Codigos/TusCodigos.pdf"
+    else:
+        return
+    
     EMAIL_ADDRESS = EMAIL
     EMAIL_PASSWORD = CONTRASEÑA
     msg = EmailMessage()
@@ -45,16 +26,13 @@ def mandarCorreoFactura(correo, pathArchivo):
 
     msg.add_alternative(html, subtype = 'html')
     
-    archivos = [pathArchivo]
-
-    for archivo in archivos:
-        with open(archivo, 'rb') as f:
-            file_data = f.read()
-            file_name = f.name
+    archivo = path
+    with open(archivo, 'rb') as f:
+        file_data = f.read()
+        file_name = f.name
 
     msg.add_attachment(file_data, maintype = 'aplication', subtype ='octect-stream' , filename = file_name)
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         smtp.send_message(msg)
-
